adbb2e5041436ed88540b47739c6a37d204677b9
Andranik <loginemail@duck.com>
2026-08-20 07:55:06 +0300

    removed test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# Шаг 1: создаём файл с данными
-with open('test_ids.txt', 'w') as f:
-    f.write('\n111\n222\n333')
-
-# Шаг 2: открываем в 'a+' и пытаемся прочитать
-with open('test_ids.txt', 'r') as f:
-    content = f.read()
-    print(f"Прочитано:{content}")
-    print(f"Длина: {len(content)}")
